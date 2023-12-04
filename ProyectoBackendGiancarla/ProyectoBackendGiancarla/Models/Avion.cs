75208c21db2e810276de49b245aca8d8b1b8de51
gianwll <cruzgiancarla@gmail.com>
2023-12-04 12:47:28 -0400

    subida de proyecto

---
+++ ProyectoBackendGiancarla/ProyectoBackendGiancarla/Models/Avion.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace ProyectoBackendGiancarla.Models
+{
+    public class Avion
+    {
+        [Key]
+        public int idAvion { get; set; }
+        public string modelo { get; set; }
+        public int peso { get; set; }
+        public string capacidad { get; set; }
+        public int numeroLicencia { get; set; }
+        public int idHangares { get; set; }
+    }
+}
