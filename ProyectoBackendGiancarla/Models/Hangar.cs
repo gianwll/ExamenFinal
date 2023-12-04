6ff07076fb6705be01b83643082c37c66fcb7bfc
gianwll <cruzgiancarla@gmail.com>
2023-12-04 14:28:12 -0400

    subido de modelado

---
+++ ProyectoBackendGiancarla/Models/Hangar.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProyectoBackendGiancarla.Models
+{
+    public class Hangar
+    {
+        [Key]
+        public int idHangares { get; set; }
+        public string capacidad { get; set; }
+        public string localizacion { get; set; }
+    }
+}
