002e97360fc547bfc20c4af9aa2e916db37bb688
gianwll <147168509+gianwll@users.noreply.github.com>
2023-12-04 15:15:40 -0400

    Piloto.cs

--- ProyectoBackendGiancarla/Models/Piloto.cs
+++ ProyectoBackendGiancarla/Models/Piloto.cs
@@ -7,7 +7,7 @@ namespace ProyectoBackendGiancarla.Models
         [Key]
         public int numeroLicencia { get; set; }
         public string restricciones { get; set; }
-        public string salario { get; set; }
+        public int salario { get; set; }
         public string turno { get; set; }
     }
 }
